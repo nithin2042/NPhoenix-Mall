4aace68992357bee441d8a8b5c9b66f8add50b76
nithin2042 <144876999+nithin2042@users.noreply.github.com>
2024-03-30 01:50:32 -0700

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+<div class="footer" id="b4">
+    <footer class = "main-footer">
+        <div class = "container">
+            <pre style="font-size:15px;">    If you have any further questions or requirements you can contact nphoenixshopping@gmail.com                                <b style="font-size:16px;"> Nphoenix - An All in One Company.</b></pre>
+        </div>
+    </footer>
+</div>
